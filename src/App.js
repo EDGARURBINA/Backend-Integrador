b3f1e112f2a82fea38d13dc078367f72d66f70ea
Insano <insano@gmail.com>
2024-11-07 10:42:40 -0600

    a

--- src/App.js
+++ src/App.js
@@ -16,10 +16,8 @@ const server = http.createServer(app);
 
 
 app.use(cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "token"],
-    credentials: true
+    origin: ["*"],
+    methods: ["GET", "POST"],
 }));
 
 
@@ -36,15 +34,9 @@ const queue = 'conection';
 // Configuración de Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization", "token"],
-        credentials: true
-    },
-    pingTimeout: 60000,
-    pingInterval: 25000,
-    connectTimeout: 5000,
-    maxHttpBufferSize: 1e6,
+        origin: ["*"],
+        methods: ["GET", "POST"],
+    }
 });
 
 // Almacenar clientes conectados
