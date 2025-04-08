e2f839e83bc04bd4bae83553b50c297bc06284e9
Insano <insano@gmail.com>
2025-04-08 10:09:08 -0600

    alertscam

--- src/services/mqtt.service.js
+++ src/services/mqtt.service.js
@@ -112,10 +112,9 @@ class MqttService {
   async handleNotificationMessage(msg) {
     try {
       const parsedMessage = JSON.parse(msg.content.toString());
-      if (parsedMessage.notification) {
-        const alerts = Array.isArray(parsedMessage.notification)
-          ? parsedMessage.notification
-          : [parsedMessage.notification];
+      if (parsedMessage.alerts) {
+        const alerts = parsedMessage.alerts;
+        console.log("Alertas recibidas:", alerts); 
 
         for (const alert of alerts) {
           const alertId = {
